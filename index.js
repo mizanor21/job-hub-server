18de6768e11fd02379abfae028d0a78ee73e39ff
mizanor21 <mizanorrahman398@gmail.com>
2023-02-18 15:31:45 +0600

    versel deploy related changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json());
-require('dotenv').config();
+// require('dotenv').config();
 
 // mongoDB user: jobHub
 // mongoDB pass: turf8DYkm7fOO55i
