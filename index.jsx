7c30b42e12cdd7418ca42ca7ecf73deaca8bf686
mizanor21 <mizanorrahman398@gmail.com>
2023-02-18 11:23:43 +0600

    fresherjobs part added

--- index.jsx
+++ index.jsx
@@ -4,6 +4,33 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
+app.use(express.json());
+require('dotenv').config();
+
+// mongoDB user: jobHub
+// mongoDB pass: turf8DYkm7fOO55i
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://jobHub:turf8DYkm7fOO55i@cluster0.disah5t.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const fresherJobCollection = client.db('jobHub').collection('fresherJobs');
+
+        app.get('/fresher-jobs', async (req, res) => {
+            const query = {};
+            const cursor = fresherJobCollection.find(query)
+            const result = await cursor.limit(6).toArray();
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Simple node is running')
